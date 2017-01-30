0db18136443a59c41c1e3763973016b84797c2e2
zeroCalm <smith.timothyh@gmail.com>
2017-01-29 18:45:34 -0800

    updated loop

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,30 +16,50 @@ $(document).ready(function(){
 });
 
 
-//////////  ADDING PROJECTS TO projectsPage   ////////////
 
 
-                  ///////MY 3 PROJECTS////////
-//var myWork = [
-//
-// {
-//   name: "Random Colors",
-//   description: "Hover/click on buttons and get a lightshow!!! HTML, CSS and JAVASCRIPT",
-//   link:  "https://github.com/ZeroCalm/Web-Page-Fun-Personal-Projects"
-// },
-//
-// {
-//   name: "Matching Cards Game",
-//   description: "Basic card matching game using HTML, CSS and JAVASCRIPT",
-//   link: "https://github.com/ga-students/wdi-fundamentals-memorygame"
-//
-// },
-//
-// {
-//   name: "Basic Algorithm practice",
-//   description: "Basic card matching game using HTML, CSS and JAVASCRIPT",
-//   link: "https://github.com/ZeroCalm/problem-set-js-basics"
-// }]
+
+
+////////  ADDING PROJECTS TO projectsPage   ////////////
+
+
+                  /////MY 3 PROJECTS////////
+var myWork = [
+
+{
+  name: "Random Colors",
+  description: "Hover/click on buttons and get a lightshow!!! HTML, CSS and JAVASCRIPT",
+  link:  "https://github.com/ZeroCalm/Web-Page-Fun-Personal-Projects"
+},
+
+{
+  name: "Matching Cards Game",
+  description: "Basic card matching game using HTML, CSS and JAVASCRIPT",
+  link: "https://github.com/ga-students/wdi-fundamentals-memorygame"
+
+},
+
+{
+  name: "Basic Algorithm practice",
+  description: "Basic card matching game using HTML, CSS and JAVASCRIPT",
+  link: "https://github.com/ZeroCalm/problem-set-js-basics"
+}]
+
+
+
+
+for (var i=0; i<myWork.length; i++){
+  var name = myWork[i].name;
+  var description = myWork[i].description;
+  var link = myWork[i].link;
+
+  var itemToShow = <a href="/'" + link + "' + "class=/'" + "description" + "/'" >
+}
+
+
+
+
+
 //
 //
 //
