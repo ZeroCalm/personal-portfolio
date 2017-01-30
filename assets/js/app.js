258fcc307b022492c57dfb30440927f353eaa9c4
zeroCalm <smith.timothyh@gmail.com>
2017-01-30 06:53:21 -0800

    trying another approach to loop, projects page still not showing on click

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,42 +20,42 @@ $(document).ready(function(){
 
 
 
-////////  ADDING PROJECTS TO projectsPage   ////////////
+////////   PROJECTS to go on projectsPage   ////////////
 
-
-                  /////MY 3 PROJECTS////////
-var myWork = [
-
-{
-  name: "Random Colors",
-  description: "Hover/click on buttons and get a lightshow!!! HTML, CSS and JAVASCRIPT",
-  link:  "https://github.com/ZeroCalm/Web-Page-Fun-Personal-Projects"
-},
-
-{
-  name: "Matching Cards Game",
-  description: "Basic card matching game using HTML, CSS and JAVASCRIPT",
-  link: "https://github.com/ga-students/wdi-fundamentals-memorygame"
-
-},
-
-{
-  name: "Basic Algorithm practice",
-  description: "Basic card matching game using HTML, CSS and JAVASCRIPT",
-  link: "https://github.com/ZeroCalm/problem-set-js-basics"
-}]
-
-
-
-
-for (var i=0; i<myWork.length; i++){
-  var name = myWork[i].name;
-  var description = myWork[i].description;
-  var link = myWork[i].link;
-
-  var itemToShow = <a href="/'" + link + "' + "class=/'" + "description" + "/'" >
-}
-
+//
+//                   /////MY 3 PROJECTS////////
+// var myWork = [
+//
+// {
+//   name: "Random Colors",
+//   description: "Hover/click on buttons and get a lightshow!!! HTML, CSS and JAVASCRIPT",
+//   link:  "https://github.com/ZeroCalm/Web-Page-Fun-Personal-Projects"
+// },
+//
+// {
+//   name: "Matching Cards Game",
+//   description: "Basic card matching game using HTML, CSS and JAVASCRIPT",
+//   link: "https://github.com/ga-students/wdi-fundamentals-memorygame"
+//
+// },
+//
+// {
+//   name: "Basic Algorithm practice",
+//   description: "Basic card matching game using HTML, CSS and JAVASCRIPT",
+//   link: "https://github.com/ZeroCalm/problem-set-js-basics"
+// }]
+//
+//
+//
+//
+// for (var i=0; i<myWork.length; i++){
+//   var name = myWork[i].name;
+//   var description = myWork[i].description;
+//   var link = myWork[i].link;
+//
+//   projectsPage.innerHTML(<a href="link" alt="description">name</a>);
+// }
+//
 
 
 
